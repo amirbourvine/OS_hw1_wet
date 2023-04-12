b8f7013dd9e25526b4aa5481d51ca84431b997d0
amirb <amirbourvine@campus.technion.ac.il>
2023-04-12 12:43:36 +0300

    signals

--- signals.cpp
+++ signals.cpp
@@ -14,7 +14,7 @@ void ctrlZHandler(int sig_num) {
     }
     else{
         smash.add_job(smash.get_foreground_job_cmd(), smash.get_foreground_job_pid(), true);
-        int err = kill(smash.get_foreground_job_pid(),SIGTSTP);
+        int err = kill(smash.get_foreground_job_pid(),SIGSTOP);
         if(err == -1){
             perror("smash error: kill failed");
             return;
