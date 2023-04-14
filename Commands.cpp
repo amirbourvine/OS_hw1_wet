96c05064c9a251ee4db1da0efe781f586b7d1051
amirb <amirbourvine@campus.technion.ac.il>
2023-04-14 11:38:19 +0300

    pipe debug

--- Commands.cpp
+++ Commands.cpp
@@ -793,8 +793,6 @@ PipeCommand::PipeCommand(const char *cmd_line) : Command(cmd_line){
     if(this->is3()) {
         this->first_cmd = temp.substr(0, temp.find_first_of('|'));
         this->second_cmd = temp.substr(temp.find_first_of('|')+1, temp.size()-temp.find_first_of('|')-1);
-        cout <<"FIRST: " << this->first_cmd <<  endl;
-        cout <<"SECOND: " << this->second_cmd <<  endl;
     }
     if(this->is4()){
         this->first_cmd = temp.substr(0, temp.find_first_of("|&"));
@@ -970,21 +968,8 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
 void SmallShell::executeCommand(const char *cmd_line, bool is_pipe_second_cmd) {
     // TODO: Add your implementation here
     // for example:
-    std::string final_cmd = cmd_line;
 
-    /*
-    if(is_pipe_second_cmd){
-        char temp[200];
-        read(0, temp, 200);
-        std::string  help = temp;
-        final_cmd += " ";
-        final_cmd += help;
-
-        cout <<"FINAL_SECOND: " << final_cmd <<  endl;
-    }
-    */
-
-    Command* cmd = CreateCommand(final_cmd.c_str());
+    Command* cmd = CreateCommand(cmd_line);
     cmd->execute();
 
     delete cmd;
